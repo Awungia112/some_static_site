6672ddc4b9e7cd65c42c7ac678fd23e244319f76
Kunga Derick Abongho <abonghoderick@gmail.com>
2025-04-12 03:43:59 +0100

    Delete target directory

--- target/debug/deps/axum_server-c2ca397867525524.d
+++
@@ -1,5 +0,0 @@
-/home/kunga-derick/projects/rust_programming/some_static_site/target/debug/deps/libaxum_server-c2ca397867525524.rmeta: src/main.rs
-
-/home/kunga-derick/projects/rust_programming/some_static_site/target/debug/deps/axum_server-c2ca397867525524.d: src/main.rs
-
-src/main.rs:
--- target/release/axum-server.d
+++
@@ -1,1 +0,0 @@
-/home/kunga-derick/projects/rust_programming/some_static_site/target/release/axum-server: /home/kunga-derick/projects/rust_programming/some_static_site/src/main.rs
